77e81e7e353f1af9a4a8f698a37ea5fb87637b9e
Federico Coraglio <fedecoraglio@hotmail.com>
2020-11-06 19:37:09 -0300

    dbstring

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // const URI = 'mongodb://localhost/chatbot';
-const URI = 'mongodb://localhost/chatbot';
+const URI = process.env.DB_STRING
 
 mongoose.connect(URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
     .then(db => {
--- setenv.js
+++ setenv.js
@@ -3,3 +3,4 @@ const path = require('path');
 process.env.PYTHON = "python"
 process.env.PYTHON_FILE = path.resolve(__dirname, "./python/init.py")
 // process.env.STATICS_FILES = path.join(__dirname, './frontend/dist');
+process.env.DB_STRING = "mongodb+srv://federico:uUmQB7B1sF5ytXg8@cluster0.ix9a2.mongodb.net/chatbot?retryWrites=true&w=majority"
